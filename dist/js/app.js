6e12f52803894c5afe2c74ce0f82c800bfc06c3a
vlad <vladslobodanuk9@gmail.com>
2022-11-08 01:20:53 +0200

    js-2

--- dist/js/app.js
+++ dist/js/app.js
@@ -170,7 +170,7 @@ const SWIPER_TARIFS = new Swiper('.swiper-tariffs', {
 const API = './js/tariffs.json';
 const MORE_BTN = document.querySelectorAll('.more')
 const POPUP = document.querySelector('#popup')
-const CLOSE = document.querySelector('.popup__close')
+
 
 let readJson = () => {
 	fetch(API)
@@ -178,6 +178,7 @@ let readJson = () => {
 		.then((data) => {
 
 			getTariffsPopup(data);
+
 
 		})
 		.catch((error) => {
@@ -193,23 +194,28 @@ let getTariffsPopup = (data) => {
 
 	for (let i = 0; i < MORE_BTN.length; i++) {
 		MORE_BTN[i].addEventListener("click", function () {
-			console.log(1)
+
 
 			POPUP.classList.add('active');
+			document.body.classList.toggle('no-scroll');
+
+
+
 			const TITLE = data[i].title;
 			const SPEED = data[i].speed;
 			const PRICE = data[i].price;
 
-			POPUP.innerHTML = `<div class="popup__body">
-			<div class="popup__content">
+			POPUP.innerHTML = `<div class="popup__body popup__close" >
+
+			<div class="popup__content ">
 				<div class="popup__header">
 					<span class="popup__title">Тариф «${TITLE}»</span>
-					<button class="popup__close">&#10006;</button>
+					<button class="popup__close"  >&#10006;</button>
 				</div>
 				<div class="popup__main popup-main">
 					<div class="popup-main__left">
 						<span class="speed">${SPEED}</span>
-						<ul>
+						<ul >
 							<li>
 								<div>
 									<svg class="icon">
@@ -227,7 +233,7 @@ let getTariffsPopup = (data) => {
 								<p>Статический IP-адрес</p>
 							</li>
 						</ul>
-						<span class="SPEED">Необходимое оборудование</span>
+						<span class="speed">Необходимое оборудование</span>
 						<ul>
 							<li>
 								<div>
@@ -279,23 +285,36 @@ let getTariffsPopup = (data) => {
 				</div>
 			</div>
 		</div>`
+			const CLOSE = document.querySelector('.popup__close')
+			console.log(CLOSE)
 
+
+			CLOSE.addEventListener("click", function (e) {
+				if (e.target.classList.contains('popup__close')) {
+					POPUP.classList.remove('active');
+					document.body.classList.remove('no-scroll')
+				}
+			});
+
+
+			// CLOSE.onclick = function () { alert('clicked "b"') }
+			// a.addEventListener("click", function (e) {
+			// 	alert('clicked a');
+
+			// 	e.stopPropagation();
+			// }, true);
 		});
+
 	}
 
-	CLOSE.addEventListener("click", function () {
 
-		POPUP.classList.remove('active');
-
-
-	});
 
 }
 
 readJson();
 
 
-// PHONE MASK
+// PHONE MASK ВАЛИДАЦИЯ 
 
 
 
@@ -339,13 +358,13 @@ form.onsubmit = function () {
 	});
 
 	if (emptyInputs.length !== 0) {
-		console.log('inputs not filled');
+
 		return false;
 	}
 
 
 	if (!validatePhone(phoneVal)) {
-		console.log('phone not valid');
+
 		inputPhone.classList.add('error');
 		return false;
 	} else {
@@ -353,7 +372,7 @@ form.onsubmit = function () {
 	}
 
 	if (!validateName(nameVal)) {
-		console.log('name not valid');
+
 		inputName.classList.add('error');
 		return false;
 	} else {
@@ -361,7 +380,6 @@ form.onsubmit = function () {
 	}
 
 	if (!inputCheckbox.checked) {
-		console.log('checkbox not checked');
 		inputCheckbox.classList.add('error');
 		return false;
 	} else {
@@ -370,4 +388,143 @@ form.onsubmit = function () {
 
 
 }
+
+
+//  ПОИСК
+
+// ВАЛИДАЦИЯ 
+
+const SEARCH = './js/connections.json';
+const BTN_SEARCH = document.querySelector('.btn-search');
+
+
+let form2 = document.querySelector('.js-form-2'),
+	formInputs2 = document.querySelectorAll('.js-input-2');
+
+
+let searchJson = () => {
+	fetch('./js/connections.json')
+		.then((response) => response.json())
+		.then((data) => {
+
+			console.log(data)
+			examination(data)
+
+		})
+		.catch((error) => {
+			console.log(error);
+		})
+}
+
+let examination = (data) => {
+	let popup = document.querySelector('.popup-search'),
+		formInputs2 = document.querySelectorAll('.js-input-2');
+
+
+	let st = document.querySelector('.js-input-st'),
+		home = document.querySelector('.js-input-home'),
+		valSt = st.value,
+		valHome = home.value;
+
+	// function validateSt(street) {
+	// 	if (!street) return street;
+
+	// 	street[0].toUpperCase() + street.slice(1);
+
+	// 	return false;
+	// }
+
+
+	formInputs2.forEach(function (input) {
+		if (input.value === '') {
+			input.classList.add('error');
+
+		} else {
+			input.classList.remove('error');
+			if (valSt !== '' && valHome !== '') {
+
+				popup.classList.add('active')
+				document.body.classList.add('no-scroll');
+
+
+				const P1 = valSt;
+				const P2 = valHome;
+				popup.innerHTML = `<div class="popup__body popup__close-2">
+								<div class="black-backing"></div>
+								<div class="popup__content">
+									<div class="popup__header">
+										<span class="popup__title">Проверка на подключение</span>
+										<button class="popup__close-2">&#10006;</button>
+									</div>
+									<div class="popup__main popup-main">
+										<p class="p">
+										По адресу <strong> ${P1}</strong> дом <strong> ${P2}</strong> возможность подключения отсутствует :(
+										</p>
+									</div>
+								</div>
+							</div>`
+
+
+				for (let i = 0; i < data.length; i++) {
+
+					if (data[i].st == valSt) {
+						data[i].home.forEach(function (item) {
+							if (item == valHome) {
+								popup.classList.add('active')
+								const P1 = valSt;
+								const P2 = valHome;
+								popup.innerHTML = `<div class="popup__body popup__close-2">
+								<div class="black-backing"></div>
+								<div class="popup__content">
+									<div class="popup__header">
+										<span class="popup__title">Проверка на подключение</span>
+										<button class="popup__close-2">&#10006;</button>
+									</div>
+									<div class="popup__main popup-main">
+										<p class="p">
+											По адресу <strong> ${P1}</strong> дом <strong> ${P2}</strong> возможноe подключение :)
+										</p>
+									</div>
+								</div>
+							</div>`
+
+							}
+						})
+					}
+				}
+			}
+
+
+		}
+	});
+
+
+	const CLOSE_2 = document.querySelector('.popup__close-2')
+
+
+
+	CLOSE_2.addEventListener("click", function (e) {
+		if (e.target.classList.contains('popup__close-2')) {
+			popup.classList.remove('active');
+			document.body.classList.remove('no-scroll')
+		}
+	});
+
+
+
+
+}
+
+BTN_SEARCH.addEventListener('click', searchJson)
+
+
+
+
+
+
+
+
+
+
+
 
